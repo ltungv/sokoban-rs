fa7ec7b89f2e70ccb7b001d40c1b900cbb75e1dd
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-04-03 00:32:13 +0700

    refactor main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,36 +9,14 @@ mod game;
 mod resources;
 mod systems;
 
-/// Window's width
 pub const ARENA_WIDTH: f32 = 720.0;
-/// Window's height
 pub const ARENA_HEIGHT: f32 = game::MAP_HEIGHT as f32 * game::TILE_HEIGHT;
 
-/// The game will contains the following entities:
-/// + Moveable entities
-///     1. Player: mint::Point3, Renderable, Moveable
-///     2. Box:    mint::Point3, Renderable, Moveable
-/// + Immoveable entities:
-///     1. Wall:     mint::Point3, Renderable
-///     2. Floor:    mint::Point3, Renderable
-///     3. Box spot: mint::Point3, Renderable
+/// Load the game's resources and initialize the game. The path to the resources
+/// is relative to the directory that contains the project's manifest, otherwise,
+/// it is relative to the current position where the project is run.
 fn main() -> ggez::GameResult {
-    let resource_dir = match env::var("CARGO_MANIFEST_DIR") {
-        Ok(manifest_dir) => {
-            let mut path = path::PathBuf::from(manifest_dir);
-            path.push("resources");
-            path
-        }
-        Err(_) => path::PathBuf::from("./resources"),
-    };
-    println!("Resource dir: {:?}", resource_dir);
-
-    let (ctx, evts_loop) = &mut ggez::ContextBuilder::new("sokoban", "tlv")
-        .window_setup(conf::WindowSetup::default().title("Sokoban"))
-        .window_mode(conf::WindowMode::default().dimensions(ARENA_WIDTH, ARENA_HEIGHT))
-        .add_resource_path(&resource_dir)
-        .build()?;
-
+    // TODO: Load map from persistence
     const MAP: &str = "\
         N N W W W W W W W
         W W W . . . . . W
@@ -50,6 +28,20 @@ fn main() -> ggez::GameResult {
         W . . . . . . . W
         W W W W W W W W W";
 
+    let resource_dir = env::var("CARGO_MANIFEST_DIR")
+        .map(|dir| {
+            let mut path = path::PathBuf::from(dir);
+            path.push("resources");
+            path
+        })
+        .unwrap_or_else(|_| path::PathBuf::from("./resources"));
+    println!("Resource dir: {:?}", resource_dir);
+
+    let (ctx, evts_loop) = &mut ggez::ContextBuilder::new("sokoban", "tlv")
+        .window_setup(conf::WindowSetup::default().title("Sokoban"))
+        .window_mode(conf::WindowMode::default().dimensions(ARENA_WIDTH, ARENA_HEIGHT))
+        .add_resource_path(&resource_dir)
+        .build()?;
     let game = &mut game::Game::new(ctx, MAP)?;
     event::run(ctx, evts_loop, game)
 }
